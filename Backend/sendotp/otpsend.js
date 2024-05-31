48ad7a68250e1f6f0d84b997c5f3e53bd5aa31d7
kdkprabhakar100 <97750806+kdkprabhakar100@users.noreply.github.com>
2024-05-31 07:49:15 +0545

    Update otpsend.js

--- Backend/sendotp/otpsend.js
+++ Backend/sendotp/otpsend.js
@@ -1,6 +1,6 @@
 const accountSid = 'ACd2501210e190bcaf2573743341408a40';
 const authToken = 'bae7a8a6d7bacf3b3acfc0efe01153a3';
-const twilioNumber = '+18064294191';
+const twilioNumber = '+18064294191';  
 
 const twilio = require('twilio')(accountSid, authToken);
 
