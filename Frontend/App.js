ded0cc02a008a1df7974b142e18e78b7a9dc0076
SushantChand <95869406+SushantChand@users.noreply.github.com>
2024-06-25 21:20:21 +0545

    Update App.js
    Signed-off-by: SushantChand <95869406+SushantChand@users.noreply.github.com>

--- Frontend/App.js
+++ Frontend/App.js
@@ -4,7 +4,6 @@ import StackNavigator from './src/StackNavigation/StackNavigator';
 import { FitnessContext } from './src/components/Context/Context';
 import { ThemeProvider } from './src/TabNavigation/ThemeContext';
 
-
 const App = () => {
   return (
     <ThemeProvider>
