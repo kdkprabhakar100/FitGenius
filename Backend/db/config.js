5d7b3aa7f21aad9511a499cee83cfa8213f6a60d
SushantChand <95869406+SushantChand@users.noreply.github.com>
2024-06-28 13:45:37 +0545

    Update config.js
    Signed-off-by: SushantChand <95869406+SushantChand@users.noreply.github.com>

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -3,5 +3,4 @@ mongoose.connect("mongodb://127.0.0.1:27017/FitGenius").then(() => {
     console.log("Database successfully connected ")
 })
 
-
 // mongoose.connect("mongodb://localhost:27017/FitGenius")
