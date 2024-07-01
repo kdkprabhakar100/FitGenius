64e1e6dafb2d1235acd16550b5543aa224554f67
kdkprabhakar100 <97750806+kdkprabhakar100@users.noreply.github.com>
2024-07-01 10:22:02 +0545

    Update config.js
    Signed-off-by: kdkprabhakar100 <97750806+kdkprabhakar100@users.noreply.github.com>

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/FitGenius").then(() => {
     console.log("Database successfully connected ")
 })
-<<<<<<< HEAD
-// mongoose.connect("mongodb://localhost:27017/FitGenius")
-=======
 
 // mongoose.connect("mongodb://localhost:27017/FitGenius")
->>>>>>> faf7ae28b1c54f834bd736f143f2af31c8315941
+
+
