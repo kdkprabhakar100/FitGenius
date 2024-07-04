1693037593ad7525c25fa889587d78636d53b432
Prabhakar Khadka <kdkprabhakar100@gmail.com>
2024-07-04 12:59:52 +0545

    fimal push

--- Frontend/src/components/ReactNativeBlePlx/BleManagerInstance.js
+++
@@ -1,3 +0,0 @@
-import { BleManager } from "@config-plugins/react-native-ble-plx"
-
-export const manager = new BleManager()
--- Frontend/src/constant/constant.js
+++
@@ -1,2 +0,0 @@
-export const default_ip_address = 'http://192.168.18.8:4000';
-export const default_email = 'manushipaudel00@gmail.com';
